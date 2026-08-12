7ade1992086458d253556f4b96417929909ce0ef
wacadey <wacadey@gmail.com>
2026-08-12 15:42:11 +0900

    Feat: kwargs 정보,메타 정보 추가

--- dags/02_basics_python.py
+++ dags/02_basics_python.py
@@ -12,6 +12,24 @@ import logging # 레벨별 로그 출력 (에러, 경고, 정보, 디버깅,..)
 
 # 2-1. 콜백함수 정의
 def _extract_cb(**kwargs):
+  '''
+  - kwargs : airflow가 작업하기 전에 내부 정보(context)를 접근할수 있는 내용 엔트리포인트
+    - context : airflow가 주입한 정보(task 정보, 시간, ... 메타 정보). task 전달데이터    
+  '''
+  # 1. 컨텍스트(airflow 내부에 정보 저장공간)에서 특정 정보 추출, 키는 정의 되어 있음
+  #    ti : <TaskInstance: 02_basics_python.extract_task manual__2026-08-12T06:37:53.765360+00:00 [running]>
+  ti        = kwargs['ti']
+  ds        = kwargs['ds']
+  ds_nodash = kwargs['ds_nodash']
+  run_id    = kwargs['run_id']  
+
+  logging.info("=== Extract 작업 ===")
+  logging.info(f" ti = {ti}")
+  logging.info(f" ds = {ds}")
+  logging.info(f" ds_nodash = {ds_nodash}")
+  logging.info(f" run_id    = {run_id}")
+
+
   pass
 def _transform_cb(**kwargs):
   pass

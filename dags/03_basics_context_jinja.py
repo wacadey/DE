c3b802910a3ef7e84df703419ecdfe1199fb56f0
wacadey <wacadey@gmail.com>
2026-08-13 10:04:17 +0900

    Feat: 접근 케이스 구성

--- dags/03_basics_context_jinja.py
+++ dags/03_basics_context_jinja.py
@@ -1,5 +1,6 @@
 '''
 - airflow 내부 정보 접근, 출력시 jinja 활용, 내부 정보 접근시 macro 활용
+- 콜백함수 내부 kwargs를 인자를 통해 접근, 기타 일반적인 상황 jinja를 이용하여 접근
 '''
 # 1. 모듈
 from airflow import DAG
@@ -12,8 +13,13 @@ import pendulum
 # 2. 전역변수
 KST = pendulum.timezone("Asia/Seoul")
 
+# 4-1. 콜백함수
+def _print(**kwargs):
+  logging.info(f'ds 출력 { kwargs["ds"] }')
+  pass
+
 # 3. DAG
-with DAG(
+with DAG( 
   dag_id      = "03_basics_context_jinja",
   description = "macro을 이용하여 context 접근, jinja를 통해 표현",
   default_args= {
@@ -29,6 +35,20 @@ with DAG(
   tags        = ['macro', 'context', 'jinja']
 ) as dag:
 
-  # 4. 오퍼레이터
+  # 4. 오퍼레이터를 이용하여 task를 정의
+  t1 = BashOperator(
+    task_id         = "jinja_used_task",
+    bash_command    = "echo 'DAG의 t1 task 수행시간 {{ ds }}, {{ ti }}' "
+  )
+  t2 = BashOperator(
+    task_id         = "jinja_macro_task",
+    # macro를 통해서 준비된 함수 활용
+    bash_command    = "echo 'DAG의 t1 task일주일전 수행시간(임시) {{ macros.ds_add(ds, -7) }}, 램덤 {{ macros.random() }}' "
+  )
+  t3 = PythonOperator(
+    task_id         = "jinja_python_task",
+    python_callable = _print
+  )
 
   # 5. 의존성(수행순서)
+  t1 >> t2 >> t3

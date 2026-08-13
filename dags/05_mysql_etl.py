fcfae664f57cb00920be812949c181a11f6a604d
wacadey <wacadey@gmail.com>
2026-08-13 13:41:49 +0900

    Feat: 데이터 transform 처리중

--- dags/05_mysql_etl.py
+++ dags/05_mysql_etl.py
@@ -25,6 +25,9 @@ import os
 # 실습 기본 구성 틀 작성
 # 2. 전역변수
 KST = pendulum.timezone("Asia/Seoul")
+# extract 한 데이터를 임시로 저장하는 위치
+DATA_PATH = "/opt/airflow/dags/data"
+os.makedirs(DATA_PATH, exist_ok=True)
 
 # 콜백함수
 def _extract(**kwargs):
@@ -42,10 +45,38 @@ def _extract(**kwargs):
     }
     for i in range(10)
   ]
-  logging.info( f'더미데이터 {data}'  )
-  pass
+  # 데이터를 다음 task에 전달 -> 데이터 전달 or 데이터 저장(리눅스 경로상)후 path 전달
+  # 파일명 sensor_data_20260813.json : 20260813=>"ds_nodash" 활용
+  # /opt/airflow/dags/data/sensor_data_DAG수행일.json
+  file_full_path = f"{DATA_PATH}/sensor_data_{kwargs['ds_nodash']}.json"
+  with open(file_full_path, 'w') as f:
+    json.dump( data, f )
+  
+  logging.info( f'extract 한 데이터 {data}'  )
+  logging.info( f'extract 한 데이터 파일 경로 {file_full_path}'  )
+  # XCOM을 통해 전달
+  return file_full_path
+
 def _transform(**kwargs):
+  # 1. _extract가 전달한 내용 XCom을 통해서 획득
+  ti              = kwargs["ti"]
+  json_file_path  = ti.xcom_pull(task_ids="extract")
+  logging.info( f'전달한 데이터 파일 경로 {json_file_path}'  )
+
+  # 2. transform => 데이터 clean, 전처리(단위변경, 파생변수, ....)
+  #    섭씨 온도를 화씨 온도로 계산 -> 파생변수 추가 -> pandas의 DataFrame 활용
+  #    섭씨 온도 100도 이하(<=)만 센서가 정상, 그 이상은 이상탐지의 대상으로 간주 -> 이상치 제거(컨셉)
+  # 2-1. json file -> load -> DataFrame
+  df = pd.read_json( json_file_path )
+  # 2-2. 이상치 제거(컨셉) -> clean 작업의 범주, 100도 이하만(조건->블리언) 추출(인덱싱):블리언 인덱싱
+  target_df = df[ df['temperature'] <= 100  ].copy()
+  # 2-3. 파생 변수 생성 -> 섭씨 => 화씨
+  #      °F = (°C × 9/5) + 32
+  target_df["temperature_f"] = (target_df['temperature'] * 9/5) + 32
+
   pass
+
+
 def _load(**kwargs):
   pass
 
